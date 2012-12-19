476bad9c02c9a14838a855aa24bc7167d60fe8d6
Rob Pamment <robin.pamment@btinternet.com>
2012-12-19 23:50:32 +0000

    move index logic into script file

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+var app = {
+    initialize: function ()
+    {
+        this.bind();
+    },
+    bind: function ()
+    {
+        document.addEventListener('deviceready', this.deviceready, false);
+    },
+    deviceready: function ()
+    {
+        // This is an event handler function, which means the scope is the event.
+        // So, we must explicitly called `app.report()` instead of `this.report()`.
+        app.connect('deviceready');
+    },
+    connect: function (id)
+    {
+        // io.Socket.prototype.isXDomain = function () { return false; };
+
+        // var socket = io.connect('http://192.168.1.101/widgetClients');
+
+        // socket.on('onTestStarted', app.onTestStarted);
+
+        $("#nodeData").text("Stuff will go here");
+    },
+    onTestStarted: function (data)
+    {
+        // Handles the test started event
+
+        $("#nodeData").text("The test is : " + data.Test);
+    }
+};
