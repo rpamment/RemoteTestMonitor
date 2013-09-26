5f8433de8a9451edcd57e4f5ce4fe4c34c729e02
Rob Pamment <robin.pamment@btinternet.com>
2013-09-26 22:27:25 +0100

    oij

--- scripts/index.js
+++ scripts/index.js
@@ -5,23 +5,28 @@ var app = {
     },
     bind: function ()
     {
-        document.addEventListener('deviceready', this.deviceready, false);
+        document.addEventListener("deviceready", this.deviceready, false);
     },
     deviceready: function ()
     {
         // This is an event handler function, which means the scope is the event.
         // So, we must explicitly called `app.report()` instead of `this.report()`.
-        app.connect('deviceready');
+        app.connect("deviceready");
     },
     connect: function (id)
     {
  //       io.Socket.prototype.isXDomain = function () { return false; };
 
-        var socket = io.connect('http://pihub-robsway.rhcloud.com:8080/piClients');
+        var socket = io.connect("http://pihub-robsway.rhcloud.com:8000/piClients");
 
-        socket.on('onTestStarted', app.onTestStarted);
+        socket.on("connect", app.onConnected);
+        socket.on("onTestStarted", app.onTestStarted);
 
         $("#runningTest").text("This has been updated!");
+    },
+    onConnected: function(data)
+    {
+        $("#runningTest").text("Connected!");        
     },
     onTestStarted: function (data)
     {
